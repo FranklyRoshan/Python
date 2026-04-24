92e963994271b47144c38d21af50ef1242a82ffa
Frank <128827930+FranklyRoshan@users.noreply.github.com>
2026-04-24 11:19:42 +0530

    main.py update

---
+++ python-projects/rain-alert-SMS/main.py
@@ -0,0 +1,51 @@
+# ===============================================================
+# Weather Alert SMS
+# ===============================================================
+
+import requests
+# pip install flask twilio
+from twilio.rest import Client
+# import smtplib
+
+# OpenWeatherMap API 
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast" # 3-hour / 5-day Forecast API
+MY_LAT = 13.082680 # Your latitude
+MY_LON = 80.270721 # Your longitude
+COUNT = 4 # API will return "COUNT" data records, each representing a 3-hour window. 
+
+weather_params = {
+    "lat": MY_LAT,
+    "lon": MY_LON,
+    "appid": WEATHER_API_KEY,
+    "cnt": COUNT,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+condition_code = [hour_data["weather"][0]["id"] for hour_data in weather_data["list"]]
+
+will_rain = False
+for item in condition_code:
+    if item < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(TWIILIO_ACCOUTN_SID, TWIILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        body="It's gonna rain 🌧️ today. Remember to bring an umbrella ☔",
+        from_= TWILIO_PHONE_NUMBER,
+        to= MY_PHONE_NUMBER,
+    )
+    # print(message.body)
+    print(message.status)
+
+    # # Weather Alert Message via Email
+    # with smtplib.SMTP("smtp.gmail.com") as connection:
+    #     connection.starttls()
+    #     connection.login(user=MY_EMAIL, password=MY_EMAIL_PASSWORD)
+    #     connection.sendmail(
+    #         from_addr=MY_EMAIL,
+    #         to_addrs=MY_EMAIL,
+    #         msg=f"subject: Weather Alert 🌧️ \n\n It's gonna rain 🌧️ today. Remember to bring an umbrella ☔ "
+    #     )
