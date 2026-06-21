69fe37219b556fa6ae69e5aa5e9ef0571d763a53
Frank <128827930+FranklyRoshan@users.noreply.github.com>
2026-06-21 11:00:15 +0530

    major_update

---
+++ concepts/001_python_syntax.py
@@ -0,0 +1,68 @@
+# --- Python Syntax ----
+"""
+Execute Python Syntax
+Python Indentation
+Indentation refers to the spaces at the beginning of a code line.
+Python uses indentation to indicate a block of code.
+Python will give you an error if you skip the indentation:
+The number of spaces is up to you as a programmer, the most common use is four, 
+but it has to be at least one.
+
+"""
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+# Python Variables
+# In Python, a variable is created when you assign a value to it:
+x = 5
+y = "Hello, World!"
+
+# Comments
+"""
+Python has commenting capability for the purpose of in-code documentation.
+Comments start with a #, and Python will render the rest of the line as a comment:
+"""
+#This is a comment.
+print("Hello, World!")
+
+
+# --- Python Statements ----
+"""
+Statements
+A computer program is a list of "instructions" to be "executed" by a computer.
+
+In a programming language, these programming instructions are called statements.
+
+The following statement prints the text "Python is fun!" to the screen:
+"""
+
+print("Python is fun!")
+
+
+"""
+In Python, a statement usually ends when the line ends. You do not need to use a semicolon (;) like in many other programming languages (for example, Java or C).
+"""
+"""
+Many Statements
+Most Python programs contain many statements.
+
+The statements are executed one by one, in the same order as they are written:
+
+"""
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+
+"""
+Semicolons (Optional, Rarely Used)
+Semicolons are optional in Python. You can write multiple statements on one line by separating them with ; but this is rarely used because it makes it hard to read:
+"""
+print("Hello"); print("How are you?"); print("Bye bye!")
+
---
+++ concepts/002_python_output.py
@@ -0,0 +1,58 @@
+# --- Print Text ---
+# Python Output / Print
+"""
+Print Text
+You have already learned that you can use the print() function to display text or output values:
+"""
+
+print("Hello World!")
+
+# You can use the print() function as many times as you want. Each call prints text on a new line by default:
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+
+"""
+Double Quotes
+Text in Python must be inside quotes. You can use either " double quotes or ' single quotes:
+"""
+print("This will work!")
+print('This will also work!')
+
+
+"""
+Print Without a New Line
+By default, the print() function ends with a new line.
+
+If you want to print multiple words on the same line, you can use the end parameter:
+"""
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+
+# --- Python Output Numbers ---
+
+"""
+Print Numbers
+You can also use the print() function to display numbers:
+
+However, unlike text, we don't put numbers inside quotes:
+"""
+print(3)
+print(358)
+print(50000)
+
+# You can also do math inside the print() function:
+
+print(3 + 3)
+print(2 * 5)
+
+"""
+Mix Text and Numbers
+You can combine text and numbers in one output by separating them with a comma:
+"""
+print("I am", 35, "years old.")
+
+
+
